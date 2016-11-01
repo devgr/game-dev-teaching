3114c52f80f343550d23833dcddae401d5b29090
George Darling <george.darling@decisionsource.com>
2016-10-31 23:20:49 -0500

    working on game form

--- gamecreator.js
+++ gamecreator.js
@@ -2,7 +2,46 @@
 	var app = new Vue({
 		el: '#app',
 		data: {
-			message: 'app test'
+			message: 'app test',
+			gameTemplates: ['Mario', 'Minecraft', 'Maze'],
+			selectedTemplate: 'Mario',
+			firstName: '',
+			lastName: '',
+		},
+		methods: {
+			filePicked: function(event){
+				var reader = new FileReader();
+				reader.onload = jsonFileLoaded;
+				reader.readAsText(event.target.files[0]);
+			},
+			saveForm: function(event){
+				// post to server
+			}
 		}
 	});
+
+
+	//var onFileUploadChange = function(event) {
+	//	var reader = new FileReader();
+	//	reader.onload = jsonFileLoaded;
+	//	reader.readAsText(event.target.files[0]);
+	//};
+
+	var jsonFileLoaded = function(event){
+		var tileJson = JSON.parse(event.target.result);
+		buildLevel(tileJson);
+	};
+
+	var buildLevel = function(tileJson){
+		window.level1 = function(){
+			background('#88aaff');
+			tilemap(tileJson, 'ss');
+			player('rocketman', 64, 64);
+			arrowkeys();
+			followcamera();
+		};
+		loadgame();
+	};
+ 
+	//document.getElementById('jsonfile').addEventListener('change', onFileUploadChange);
 })();
